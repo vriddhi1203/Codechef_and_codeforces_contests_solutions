11b66bf466c5312d048b55a515e2e6b68f133dff
Vriddhi Bhardwaj <68302775+vriddhi1203@users.noreply.github.com>
2020-10-14 23:48:20 +0530

    Update chef_and_easy_queries.cpp

--- chef_and_easy_queries.cpp
+++ chef_and_easy_queries.cpp
@@ -5,21 +5,22 @@ int main() {
     int t;
     cin>>t;
     while(t--){
-        long long int n,k,index=0,left=0,flag=0;
+        long long int n,k,index=0,left=0,flag=false;
 	    int arr[100000]={};
 	    cin>>n>>k;
 	    for(int j=1;j<=n;j++){
             cin>>arr[j];
         }
-	    for(int j=1;j<=100000;j++){
-            if(arr[j]+left<k){
-                flag=1;
+	    for(int j=1;j<=n;j++){
+            if(arr[j]+left<k && !flag){
+                flag=true;
                 index=j;
 	        }
             left=(arr[j]+left)-k;
-            if(flag==1)
-            break;
 	    }
+            if(!flag){
+            index=n+left/k+1;
+        }
 	    cout<<index<<endl;
     }
 	return 0;
