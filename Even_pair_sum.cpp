a6fc40fd480c42deda8bdd5dc09ff270d9da9e0e
Vriddhi Bhardwaj <68302775+vriddhi1203@users.noreply.github.com>
2020-12-15 23:44:59 +0530

    Add files via upload

--- Even_pair_sum.cpp
+++ Even_pair_sum.cpp
@@ -7,8 +7,8 @@ int main(){
 		long long int a,b,count=0;
 		cin>>a>>b;
 		long long int e1,e2,o1,o2;
-		e1=a/2;
-		o1=a-e1;
+		e1=a/2;  //number of even numbers
+		o1=a-e1; //number of odd numbers
 		e2=b/2;
 		o2=b-e2;
 		count=e1*e2+o1*o2;
