ea49419d285584e2f640493e35310ba638c0d329
Vriddhi Bhardwaj <68302775+vriddhi1203@users.noreply.github.com>
2020-12-27 00:34:56 +0530

    Add files via upload

---
+++ sed sequences.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		long long int n,k;
+		cin>>n>>k;
+		int arr[100000];
+		for(int i=0;i<n;i++)
+			cin>>arr[i];
+		int sum=0;
+		for(int i=0;i<n;i++){
+			sum+=arr[i];
+		}
+		if(sum%k==0)
+			cout<<"0"<<endl;
+		else
+			cout<<"1"<<endl;
+
+	}
+	return 0;
+}
