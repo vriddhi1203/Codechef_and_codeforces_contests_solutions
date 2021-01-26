01d016efbbcaf789d42f8aef4ebcdcd1bc1f1221
Vriddhi Bhardwaj <68302775+vriddhi1203@users.noreply.github.com>
2021-01-26 18:24:38 +0530

    Add files via upload

---
+++ codeforces- odd divisor.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		long long int n;
+		cin>>n;
+		int flag=0;
+		if ((n & (n - 1))==0) {
+    cout << "NO\n";
+  } else {
+    cout << "YES\n";
+  }
+	}
+	return 0;
+}
