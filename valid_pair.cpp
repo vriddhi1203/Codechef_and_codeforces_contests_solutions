80eaa67b9336bce2b677ebf74176fe29af2af400
Vriddhi Bhardwaj <68302775+vriddhi1203@users.noreply.github.com>
2021-04-05 01:18:22 +0530

    Add files via upload

---
+++ valid_pair.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main(){
+	ll a,b,c;
+	cin>>a>>b>>c;
+	if(a==b || a==c || b==c)
+		cout<<"YES"<<endl;
+	else
+		cout<<"NO"<<endl;
+	return 0;
+}
